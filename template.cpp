be540df3ca0016f2cb5047fe03dc72cc8d6d5a16
TechLead-War <ayushkapri.richard@gmail.com>
2023-08-25 08:28:32 +0530

    adding template

---
+++ template.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define per(i, a, b) for(int i = (b)-1; i >= (a); --i)
+#define trav(x, v) for(auto& x : v)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+template<typename T>
+void print(const T& container) {
+    for (const auto& x : container) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+template<typename T>
+void read(T& x) {
+    cin >> x;
+}
+
+template<typename T>
+void read(vector<T>& v) {
+    for (auto& x : v) {
+        cin >> x;
+    }
+}
+
+int main() {
+    fast_io
+    
+    int t;
+    read(t);
+    while (t--) {
+        int n;
+        read(n);
+        
+        // Your code here
+        
+    }
+    
+    return 0;
+}
